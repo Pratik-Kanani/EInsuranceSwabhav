3c9a1331b7d6ee47e25e2cacee9e50d4057d251e
rohitjaiswal08 <jaiswalrohit326@gmil.com>
2022-10-08 16:13:19 +0530

    updated agent add api

---
+++ DotnetAPI/EInsuranceAPI/Models/Agent.cs
@@ -0,0 +1,19 @@
+﻿using EInsuranceAPI.CommonLibrary.Interfaces;
+using System;
+
+namespace EInsuranceAPI.Models
+{
+
+	public class Agent: IUser
+	{
+        public System.Guid ID { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Qualification { get; set; }
+        public Nullable<bool> Status { get; set; }
+        
+	}
+}
