082c65eaa2370cc636c93dc4e2faea0a3dbe3f45
rohitjaiswal08 <jaiswalrohit326@gmil.com>
2022-10-09 13:32:33 +0530

    created AgentComminsionWithdrawl model

---
+++ Angular/EInsuranceApp/src/app/models/agent-commission-withdrawl.model.ts
@@ -0,0 +1,8 @@
+export interface AgentCommissionWithdrawl{
+    //for agent commission withdrawl records 
+        date:string;
+        particulars:string;
+        amount:number;
+        status:boolean
+        }
+        
