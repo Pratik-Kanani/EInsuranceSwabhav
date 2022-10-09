c43683a164f75ba52adee35560fb5fe7f71210f7
rohitjaiswal08 <jaiswalrohit326@gmil.com>
2022-10-09 13:26:53 +0530

    created AgentComminsion model

---
+++ Angular/EInsuranceApp/src/app/models/agent-commission.model.ts
@@ -0,0 +1,10 @@
+export interface AgentCommission{
+//for agent commission records 
+    account_no:string;    
+    name:string;
+    date:string;
+    cus_name:string;
+    insurance_scheme:string;
+    amount:number
+    }
+    
