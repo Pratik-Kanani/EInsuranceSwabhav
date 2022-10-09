46355bc29c2e6d06b05f469ad0338433eb96e156
rohitjaiswal08 <jaiswalrohit326@gmil.com>
2022-10-09 16:49:04 +0530

    updated admin.cs

--- DotnetAPI/EInsuranceAPI/Models/Agent.cs
+++ DotnetAPI/EInsuranceAPI/Models/Agent.cs
@@ -3,10 +3,14 @@
     public class Agent
     {
         public int Id {get;set;}
-        public string FirstName {get;set;}
-        public string LastName {get;set;}
-        public string UserName {get;set;}
+        public string Name {get;set;}
+        public string Code {get;set;}
         public string Password {get;set;}
+        public string ConfirmPassword {get;set;}
+        public string Address { get;set;}
+        public string Email {get;set;}
+        public string Qualification { get;set;}
+        public bool Status { get;set;}
         public virtual Employee Employee {get;set;}
     }
 }
